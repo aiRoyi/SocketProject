58c864e9ea00144a5f68102e0566b20e20b4f4b0
chenmeiyi <a4527943@qq.com>
2016-11-29 00:23:07 +0800

    add project

---
+++ server-client/SocketServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    public class Program
+    {
+        private static byte[] buffer = new byte[1024];
+        private static int port = 8888;
+        static Socket serverSocket;
+        static void Main(string[] args)
+        {
+            serverSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+            serverSocket.Listen(10);
+            Thread thread = new Thread(ListenClientConnect);
+            thread.Start();
+            Console.ReadLine();
+        }
+
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                clientSocket.Send(Encoding.ASCII.GetBytes("Server Say Hello"));
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start(clientSocket);
+            }
+        }
+
+        private static void ReceiveMessage(object socket)
+        {
+            Socket clientSocket = (Socket)socket;
+            while (true)
+            {
+                try
+                {
+                    int receiveMsg = clientSocket.Receive(buffer);
+                    Console.WriteLine("receive client message:"+Encoding.ASCII.GetString(buffer,0,receiveMsg));
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ server-client/TestClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    class Program
+    {
+        private static byte[] buffer = new byte[1024];
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888));
+            Console.WriteLine("Connect to server");
+            int receiveLength = clientSocket.Receive(buffer);
+            Console.WriteLine("receive server message:" + Encoding.ASCII.GetString(buffer, 0, receiveLength));
+            Console.WriteLine("input your message:");
+            while(true)
+            {
+                string sendMessage = Console.ReadLine();
+                if (sendMessage != null)
+                {
+                    clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+                    Console.WriteLine("send to server:" + sendMessage);
+                }
+            }
+        }
+    }
+}
